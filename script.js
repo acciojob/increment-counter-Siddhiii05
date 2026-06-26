28bf2cb9fc7005ec7d9a5e74f976a42d0f6d8ed0
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-26 19:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let count = 0;
+
+const counter = document.getElementById("counter");
+const button = document.getElementById("incrementBtn");
+
+button.addEventListener("click", function () {
+	alert(count);
+	count++;
+	counter.textContent = count;
+})
